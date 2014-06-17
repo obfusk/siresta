1fb9e090f8fe92a67839d040ecf23af200c85147
Felix C. Stegerman <flx@obfusk.net>
2014-06-17 22:25:52 +0200

    update examples; WIP

--- examples/buckets/buckets.rb
+++ examples/buckets/buckets.rb
@@ -19,4 +19,53 @@ module Buckets
 
   API     = Siresta.api
   Client  = Siresta.client
+
+  class API
+    # ... TODO ...
+
+    handle :list_buckets do
+    end
+
+    handle :create_bucket do
+    end
+
+    handle :get_bucket do
+    end
+
+    handle :update_bucket do
+    end
+
+    handle :delete_bucket do
+    end
+
+    handle :list_bucket_objects do
+    end
+
+    handle :create_bucket_object do
+    end
+
+    handle :get_bucket_object do
+    end
+
+    handle :update_bucket_object do
+    end
+
+    handle :delete_bucket_object do
+    end
+
+    handle :list_users do
+    end
+
+    handle :create_user do
+    end
+
+    handle :get_user do
+    end
+
+    handle :update_user do
+    end
+
+    handle :delete_user do
+    end
+  end
 end
